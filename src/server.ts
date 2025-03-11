461b19fbaecc34620cf1bc4e0bc04066dca93e41
bauti <bautistamendibe10@gmail.com>
2025-03-10 21:25:32 -0300

    metodo get /todo para obtener los todos

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,40 @@
 import express, { NextFunction, Request, Response} from 'express';
 import cors from 'cors';
 import axios from 'axios';
+import { Todo } from './models/todo.model';
+import dotenv from 'dotenv';
 
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
 
 app.use(cors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(error.message);
     res.status(500).json('Error del servidor');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server running and listen in port: ${port}`);
 });
 
 
-let todos: Todo[] = [
-    {
-        userId: 3,
-        id: 1,
-        title: 'Aprendiento rest api',
-        completed: false
-    },
-    {
-        userId: 4,
-        id: 2,
-        title: 'TypeScripts con express',
-        completed: true
-    },  
-]
-
-
 // Routers
-//app.get('/todos', async (req: Request, res: Response) => {
-//    
-//    const endpoint = process.env.URL_API ?? '';
-//    const post: Todo[] = await axios.get(endpoint);
-//
-//    res.send(post)
-//
-//});
+app.get('/todos', async (req: Request, res: Response) => {
+    
+    const endpoint = process.env.URL_API ?? '';
+    console.log(endpoint);
+
+    try {
+        const response = await axios.get(endpoint);
+        res.send(response.data);
+    } catch (error) {
+        console.log(error);
+        res.send(500).json(error);
+    }
+
+});
 
